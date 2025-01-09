b0e91e37ffce63f1e81e053aacb878147d793600
sbamed27 <sbamed27@gmail.com>
2025-01-09 02:04:26 +0100

    added web search service

--- server/main.py
+++ server/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from pydantic_models.chat_bot import ChatBot
+from services.search import Search
 
 app = FastAPI()
+search = Search()
 
 @app.get('/')
 def splash():
@@ -9,5 +11,5 @@ def splash():
 
 @app.post('/chat')
 def chat_endpoint(body: ChatBot):
-    print(body.query)
-    return body.query
+    search_results = search.web_search(body.query)
+    return search_results
