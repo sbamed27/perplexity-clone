1e96e8d2362141f6be423901e16f9bf364b35e56
sbamed27 <sbamed27@gmail.com>
2025-01-26 03:02:30 +0100

    fixing web scraping

--- server/services/search.py
+++ server/services/search.py
@@ -8,15 +8,15 @@ tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
 class Search():
     def web_search(self, query: str):
         results = []
-        response = tavily_client.search(query, max_results=6)
+        response = tavily_client.search(query, max_results=7)
         search_results = response.get("results", [])
         for result in search_results:
             downloaded = trafilatura.fetch_url(result.get("url"))
             content = trafilatura.extract(downloaded, include_comments=False)
-            results.append({
-                "title": result.get("title"),
-                "url": result.get("url"),
-                "content": content
-
-            })
+            if content != None:
+                results.append({
+                    "title": result.get("title"),
+                    "url": result.get("url"),
+                    "content": content
+                })
         return results
