ac5998d0d0b55e1aac721cf2144aa415e47dd338
sbamed27 <sbamed27@gmail.com>
2025-01-27 00:37:32 +0100

    added skeletonizer & made sources clickable

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Perplexity Clone',
       theme: ThemeData(
         textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
--- lib/widgets/sources_section.dart
+++ lib/widgets/sources_section.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:perplexity_clone/services/chat_web_service.dart';
 import 'package:perplexity_clone/theme/colors.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SourcesSection extends StatefulWidget {
   const SourcesSection({super.key});
@@ -10,7 +12,24 @@ class SourcesSection extends StatefulWidget {
 }
 
 class _SourcesSectionState extends State<SourcesSection> {
-  List searchResults = [];
+  bool isLoading = true;
+  List searchResults = [
+    {
+      'title': 'Ind vs Aus Live Score 4th Test',
+      'url':
+          'https://www.moneycontrol.com/sports/cricket/ind-vs-aus-live-score-4th-test-shubman-gill-dropped-australia-win-toss-opt-to-bat-liveblog-12897631.html',
+    },
+    {
+      'title': 'Ind vs Aus Live Boxing Day Test',
+      'url':
+          'https://timesofindia.indiatimes.com/sports/cricket/india-vs-australia-live-score-boxing-day-test-2024-ind-vs-aus-4th-test-day-1-live-streaming-online/liveblog/116663401.cms',
+    },
+    {
+      'title': 'Ind vs Aus - 4 Australian Batters Score Half Centuries',
+      'url':
+          'https://economictimes.indiatimes.com/news/sports/ind-vs-aus-four-australian-batters-score-half-centuries-in-boxing-day-test-jasprit-bumrah-leads-indias-fightback/articleshow/116674365.cms',
+    },
+  ];
 
   @override
   void initState() {
@@ -18,6 +37,7 @@ class _SourcesSectionState extends State<SourcesSection> {
     ChatWebService().searchResultsStream.listen((data) {
       setState(() {
         searchResults = data['data'];
+        isLoading = false;
       });
     });
   }
@@ -48,42 +68,55 @@ class _SourcesSectionState extends State<SourcesSection> {
           ]),
         ),
         const SizedBox(height: 16),
-        Wrap(
-          spacing: 16,
-          runSpacing: 16,
-          children: searchResults.map((res) {
-            return Container(
-              width: 150,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: AppColors.cardColor,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    res['title'],
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+        Skeletonizer(
+          enabled: isLoading,
+          child: Wrap(
+            spacing: 16,
+            runSpacing: 16,
+            children: searchResults.map((res) {
+              return InkWell(
+                onTap: () async {
+                  final Uri url = Uri.parse(res["url"]);
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url, mode: LaunchMode.externalApplication);
+                  } else {
+                    throw 'Could not open $url';
+                  }
+                },
+                child: Container(
+                  width: 150,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: AppColors.cardColor,
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  const SizedBox(height: 8.0),
-                  Text(
-                    res['url'],
-                    style: const TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 12,
-                    ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                  child: Column(
+                    children: [
+                      Text(
+                        res['title'],
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        res['url'],
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          }).toList(),
+                ),
+              );
+            }).toList(),
+          ),
         )
       ],
     );
